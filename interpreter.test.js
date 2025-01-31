56b1a46f03e1fd300e7edfbe93bfb8a83e775603
NatashaC477 <ncordovadiba@gmail.com>
2025-01-31 00:41:31 -0800

    Add prettier.json file to configuration

--- interpreter.test.js
+++ interpreter.test.js
@@ -1,6 +1,6 @@
 import { describe, it } from 'node:test';
 import { deepEqual } from 'node:assert/strict';
-import { parse } from 'node:path';
+import { parse } from './lion-code.js';
 
 describe ('Interpreter', () => {
     it("parses correctly", () => {
--- lion-code.js
+++ lion-code.js
@@ -1,21 +1,26 @@
 // An interpreter for the LionCode programming language.
+// import * as ohm from "ohm-js";
 
-import * as ohm from "ohm-js";
+// const grammar = ohm.grammar(` LionCode {
+//     Program = Statement+
+//     Statement = PrintStatement | AssignmentStatement
+//     PrintStatement = "print" Value
+//     AssignmentStatement = Variable "=" Value
+//     Value = Variable | Number
+//     Variable = letter (letter | digit)*
+//     Number = digit+
+//     space += " " | "\t"
+//     }`);
 
-const grammar = ohm.grammar(` LionCode {
-    Program = Statement+
-    Statement = PrintStatement | AssignmentStatement
-    PrintStatement = "print" Value
-    AssignmentStatement = Variable "=" Value
-    Value = Variable | Number
-    Variable = letter (letter | digit)*
-    Number = digit+
-    space += " " | "\t"
-    }`);
+// const match = grammar.match("print x").succeeded(); // true
+// if (match.failed()) {
+//     console.error(match.message);
+// } else {
+//     console.log("Yay! The code is valid!");
+// }
 
-const match = grammar.match("print x").succeeded(); // true
-if (match.failed()) {
-    console.error(match.message);
-} else {
-    console.log("Yay! The code is valid!");
+console.log("Welcome to the LionCode interpreter! I am currently under construction. 🚧 👷🏿‍♀️ Please check back later.");
+
+export function parse(input) {
+    return "I don't work";
 }
